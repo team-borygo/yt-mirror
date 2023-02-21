f4c449ecdd2403bec8a68f214e65ae045e7b8b3d
Soanvig <soanvig@gmail.com>
2023-02-21 22:42:02 +0100

    refactor: move command handlers to dedicated functions

--- src/main.rs
+++ src/main.rs
@@ -15,28 +15,65 @@ fn main() -> Result<()> {
     let cli = Cli {};
     let program = cli.run();
 
+    match program.command {
+        CliCommand::Prepare { processes, bookmarks } => command_prepare(
+            processes,
+            bookmarks
+        ),
+        CliCommand::Synchronize { processes, target, tmp, filter } => command_synchronize(
+            processes,
+            target,
+            tmp,
+            filter
+        ),
+        CliCommand::Failed { processes, short } => command_failed(
+            processes,
+            short
+        ),
+    }
+}
+
+fn command_prepare(
+    processes: String,
+    bookmarks: String
+) -> Result<()> {
+    // TODO: add support for different bookmarks
+    // "Bookmarks" -> loadBookmarks $ CR.ChromeRepository bookmarksPath
+    // "Bookmarks.json" -> loadBookmarks $ CR.ChromeRepository bookmarksPath
+    // "places.sqlite" -> loadBookmarks $ FR.FirefoxRepository bookmarksPath
     let library: Box<dyn Library> = Box::new(ChromiumLibrary {});
 
-    match program.command {
-        CliCommand::Prepare { processes, bookmarks } => {
-            let process_list: Vec<Process> = library
-                .get_bookmarks(Path::new(&bookmarks))?
-                .into_iter()
-                .filter_map(|b| {
-                    let video_id = get_youtube_video_id(&b.url).unwrap_or(None);
+    let process_list: Vec<Process> = library
+        .get_bookmarks(Path::new(&bookmarks))?
+        .into_iter()
+        .filter_map(|b| {
+            let video_id = get_youtube_video_id(&b.url).unwrap_or(None);
 
-                    video_id.and_then(|id| Some(bookmark_to_process(&b, id)))
-                })
-                .collect();
-        
-            let mut process_repository = ProcessRepository::new(&processes)?;
+            video_id.and_then(|id| Some(bookmark_to_process(&b, id)))
+        })
+        .collect();
 
-            process_repository.save_many(&process_list)?;
-        }
-        _ => todo!()
-    }
+    let mut process_repository = ProcessRepository::new(&processes)?;
 
-    return Ok(());
+    process_repository.save_many(&process_list)?;
+
+    Ok(())
+}
+
+fn command_synchronize(
+    processes: String,
+    target: String,
+    tmp: String,
+    filter: Option<String>,
+) -> Result<()> {
+    todo!()
+}
+
+fn command_failed(
+    processes: String,
+    short: bool
+) -> Result<()> {
+    todo!()
 }
 
 fn bookmark_to_process(_bookmark: &Bookmark, youtube_id: String) -> Process {
