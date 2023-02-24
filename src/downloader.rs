1a2f4c38d9c80c7099dff5d5d3fdf645fc678ded
Mateusz Koteja <mateusz.koteja@leocode.com>
2023-02-24 08:17:31 +0100

    wip: UI

--- src/downloader.rs
+++ src/downloader.rs
@@ -1,86 +1,56 @@
-use std::{process::{Command}};
+use std::process::Command;
 
-use anyhow::{Result};
+use anyhow::Result;
 use crossbeam_channel::{Receiver, Sender};
 use nanoid::nanoid;
 
 #[derive(Debug)]
 pub enum DownloadStatus {
-    DownloadSkipped { youtube_id: String },
-    DownloadFailed { youtube_id: String, error_message: String },
-    DownloadFinished { youtube_id: String },
+    DownloadSkipped {
+        downloader_id: String,
+        youtube_id: String,
+    },
+    DownloadFailed {
+        downloader_id: String,
+        youtube_id: String,
+        error_message: String,
+    },
+    DownloadFinished {
+        downloader_id: String,
+        youtube_id: String,
+    },
 }
 
 #[derive(Debug)]
-pub struct DownloaderResult {
-    pub actor_id: String,
-    pub status: DownloadStatus,
-}
-
-pub fn download_yt(
-    youtube_id: String,
-    target_dir: &str,
-    tmp_dir: &str,
-    match_filter: &Option<String>,
-) -> Result<DownloadStatus> {
-    let output = {
-        if cfg!(target_os = "windows") {
-            todo!("Windows is not supported")
-        } else {
-            let mut args = vec![
-                "-x".to_string(),
-                "-o".to_string(),
-                format!("{}/%(title)s.%(ext)s", tmp_dir),
-                "--no-warnings".to_string(),
-                "--exec".to_string(),
-                format!("mv {{}} {}/", target_dir)
-            ];
-
-            if let Some(filter) = match_filter {
-                args.push("--match-filter".to_string());
-                args.push(filter.to_string());
-            }
-
-            args.push("--".to_string());
-            args.push(youtube_id.clone());
-
-            Command::new("yt-dlp")
-                .args(args)
-                .output()?
-        }
-    };
-
-    let stdout = String::from_utf8(output.stdout)?;
-    let stderr = String::from_utf8(output.stderr)?;
-    let is_skipped = stdout.starts_with("skipping ..");
-
-    if output.status.success() {
-        if is_skipped {
-            Ok(DownloadStatus::DownloadSkipped { youtube_id })
-        } else {
-            Ok(DownloadStatus::DownloadFinished { youtube_id })
-        }
-    } else {
-        Ok(DownloadStatus::DownloadFailed { youtube_id, error_message: stderr })
-    }
+pub enum DownloaderState {
+    Downloading {
+        downloader_id: String,
+        youtube_id: String,
+    },
+    Finished {
+        downloader_id: String,
+    },
+    Waiting {
+        downloader_id: String,
+    },
 }
 
 pub struct Downloader {
     id: String,
     work_channel: Receiver<String>,
-    result_channel: Sender<DownloaderResult>,
+    result_channel: Sender<DownloadStatus>,
     target: String,
     tmp: String,
-    filter: Option<String>
+    filter: Option<String>,
 }
 
 impl Downloader {
     pub fn new(
         work_channel: Receiver<String>,
-        result_channel: Sender<DownloaderResult>,
+        result_channel: Sender<DownloadStatus>,
         target: String,
         tmp: String,
-        filter: Option<String>
+        filter: Option<String>,
     ) -> Self {
         Downloader {
             id: nanoid!(5),
@@ -94,28 +64,75 @@ impl Downloader {
 
     pub fn start(&self) -> () {
         while let Ok(youtube_id) = self.work_channel.recv() {
-            let result = download_yt(
-                youtube_id,
-                &self.target,
-                &self.tmp,
-                &self.filter
-            );
-            
-            match result {
-                Ok(status) => {
-                    let result = DownloaderResult {
-                        actor_id: self.id.to_string(),
-                        status,
-                    };
+            let result = self.download_yt(youtube_id, &self.target, &self.tmp, &self.filter);
 
-                    self.result_channel.send(result).expect(
-                        "Cannot send download result to result channel"
-                    );
+            match result {
+                Ok(result) => {
+                    self.result_channel
+                        .send(result)
+                        .expect("Cannot send download result to result channel");
                 }
                 Err(_) => {
                     todo!()
                 }
             }
+        }
+    }
+
+    pub fn download_yt(
+        &self,
+        youtube_id: String,
+        target_dir: &str,
+        tmp_dir: &str,
+        match_filter: &Option<String>,
+    ) -> Result<DownloadStatus> {
+        let output = {
+            if cfg!(target_os = "windows") {
+                todo!("Windows is not supported")
+            } else {
+                let mut args = vec![
+                    "-x".to_string(),
+                    "-o".to_string(),
+                    format!("{}/%(title)s.%(ext)s", tmp_dir),
+                    "--no-warnings".to_string(),
+                    "--exec".to_string(),
+                    format!("mv {{}} {}/", target_dir),
+                ];
+
+                if let Some(filter) = match_filter {
+                    args.push("--match-filter".to_string());
+                    args.push(filter.to_string());
+                }
+
+                args.push("--".to_string());
+                args.push(youtube_id.clone());
+
+                Command::new("yt-dlp").args(args).output()?
+            }
+        };
+
+        let stdout = String::from_utf8(output.stdout)?;
+        let stderr = String::from_utf8(output.stderr)?;
+        let is_skipped = stdout.starts_with("skipping ..");
+
+        if output.status.success() {
+            if is_skipped {
+                Ok(DownloadStatus::DownloadSkipped {
+                    youtube_id,
+                    downloader_id: self.id.clone(),
+                })
+            } else {
+                Ok(DownloadStatus::DownloadFinished {
+                    youtube_id,
+                    downloader_id: self.id.clone(),
+                })
+            }
+        } else {
+            Ok(DownloadStatus::DownloadFailed {
+                youtube_id,
+                error_message: stderr,
+                downloader_id: self.id.clone(),
+            })
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,134 @@
-use std::{path::Path, thread::{self}, time, panic};
+use std::{
+    collections::HashMap,
+    io,
+    path::Path,
+    thread::{self},
+    time::Duration,
+};
 
 use anyhow::Result;
-use downloader::{Downloader};
+use crossterm::{
+    event::DisableMouseCapture,
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use downloader::{Downloader, DownloaderState};
 use library::firefox_library::FirefoxLibrary;
+use tui::{
+    backend::CrosstermBackend,
+    layout::{Constraint, Direction, Layout},
+    style::{Color, Modifier, Style},
+    text::{Span, Spans},
+    widgets::{Block, Borders, Gauge, Paragraph},
+    Terminal,
+};
 use types::{Process, ProcessState};
 
-use crate::{api::cli::{Cli, CliCommand}, library::{chromium_library::ChromiumLibrary, Library}, youtube::get_youtube_video_id, types::Bookmark, process_repository::ProcessRepository, downloader::DownloadStatus};
+use crate::{
+    api::cli::{Cli, CliCommand},
+    downloader::DownloadStatus,
+    library::{chromium_library::ChromiumLibrary, Library},
+    process_repository::ProcessRepository,
+    types::Bookmark,
+    youtube::get_youtube_video_id,
+};
 
 mod api;
+mod downloader;
 mod library;
+mod process_repository;
 mod types;
 mod youtube;
-mod process_repository;
-mod downloader;
 
 fn main() -> Result<()> {
+    draw_ui()?;
+
+    return Ok(());
+
     let cli = Cli {};
     let program = cli.run();
 
     match program.command {
-        CliCommand::Prepare { processes, bookmarks } => command_prepare(
+        CliCommand::Prepare {
             processes,
-            bookmarks
-        ),
-        CliCommand::Synchronize { processes, target, tmp, filter } => command_synchronize(
+            bookmarks,
+        } => command_prepare(processes, bookmarks),
+        CliCommand::Synchronize {
             processes,
             target,
             tmp,
-            filter
-        ),
-        CliCommand::Failed { processes, short } => command_failed(
-            processes,
-            short
-        ),
+            filter,
+        } => command_synchronize(processes, target, tmp, filter),
+        CliCommand::Failed { processes, short } => command_failed(processes, short),
     }
 }
 
-fn command_prepare(
-    processes: String,
-    bookmarks: String
-) -> Result<()> {
+fn draw_ui() -> Result<()> {
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    let mut downloader_states: HashMap<String, DownloaderState> = HashMap::new();
+
+    terminal.draw(|f| {
+        let layout = Layout::default()
+            .direction(Direction::Vertical)
+            .margin(1)
+            .constraints([Constraint::Length(3), Constraint::Percentage(100)].as_ref())
+            .split(f.size());
+
+        let progress_block = Gauge::default()
+            .block(Block::default().borders(Borders::ALL).title("Progress"))
+            .gauge_style(Style::default().fg(Color::White).bg(Color::Black))
+            .percent(20);
+
+        let actors_column_text: Vec<Spans> = downloader_states
+            .values()
+            .map(|state| match state {
+                DownloaderState::Downloading {
+                    downloader_id,
+                    youtube_id,
+                } => format!("{}: Downloading {}", downloader_id, youtube_id).to_string(),
+                DownloaderState::Finished { downloader_id } => {
+                    format!("{}: Finished", downloader_id).to_string()
+                }
+                DownloaderState::Waiting { downloader_id } => {
+                    format!("{}: Waiting", downloader_id).to_string()
+                }
+            })
+            .map(|str| Spans::from(Span::raw(str)))
+            .collect();
+
+        let actor_block = Paragraph::new(actors_column_text)
+            .block(Block::default().title("Block 2").borders(Borders::ALL));
+
+        f.render_widget(progress_block, layout[0]);
+        f.render_widget(actor_block, layout[1]);
+    })?;
+
+    thread::sleep(Duration::from_millis(5000));
+
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    Ok(())
+}
+
+fn command_prepare(processes: String, bookmarks: String) -> Result<()> {
     let bookmarks_file = Path::new(&bookmarks).file_name().and_then(|f| f.to_str());
 
     let library: Box<dyn Library> = match bookmarks_file {
         Some("Bookmarks") => Box::new(ChromiumLibrary {}),
         Some("Bookmarks.json") => Box::new(ChromiumLibrary {}),
         Some("places.sqlite") => Box::new(FirefoxLibrary {}),
-        _ => todo!()
+        _ => todo!(),
     };
 
     let process_list: Vec<Process> = library
@@ -93,7 +175,7 @@ fn command_synchronize(
             result_channel_s.clone(),
             target.clone(),
             tmp.clone(),
-            filter.clone()
+            filter.clone(),
         );
         let handle = thread::spawn(move || {
             downloader.start();
@@ -106,14 +188,22 @@ fn command_synchronize(
     drop(process_channel_s);
 
     while let Ok(result) = result_channel_r.recv() {
-        match &result.status {
-            DownloadStatus::DownloadFailed { youtube_id, error_message } => {
-                process_repository.fail(&youtube_id, &error_message)
-            }
-            DownloadStatus::DownloadFinished { youtube_id } => {
+        match &result {
+            DownloadStatus::DownloadFailed {
+                youtube_id,
+                error_message,
+                downloader_id,
+            } => process_repository.fail(&youtube_id, &error_message),
+            DownloadStatus::DownloadFinished {
+                youtube_id,
+                downloader_id,
+            } => {
                 process_repository.finish(&youtube_id);
             }
-            DownloadStatus::DownloadSkipped { youtube_id } => {
+            DownloadStatus::DownloadSkipped {
+                youtube_id,
+                downloader_id,
+            } => {
                 process_repository.skip(&youtube_id);
             }
         }
@@ -132,10 +222,7 @@ fn command_synchronize(
     Ok(())
 }
 
-fn command_failed(
-    processes: String,
-    short: bool
-) -> Result<()> {
+fn command_failed(processes: String, short: bool) -> Result<()> {
     let process_repository = ProcessRepository::new(&processes)?;
 
     let pending = process_repository.get_by_state(ProcessState::Failed)?;
