cf6324303295d34226c23c8bbea1f975607a6455
Soanvig <soanvig@gmail.com>
2023-04-18 20:07:55 +0200

    chore: add simple AppConfig

--- src/config/app_config.rs
+++ src/config/app_config.rs
@@ -1,2 +1,16 @@
-// Configuration file
-struct AppConfig {}
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize)]
+pub struct AppConfig {
+    pub(super) bookmark_files: Vec<String>,
+    pub(super) tmp_dir: Option<String>,
+}
+
+impl AppConfig {
+    pub fn new_default() -> AppConfig {
+        AppConfig {
+            bookmark_files: vec![],
+            tmp_dir: Some("/tmp".to_string()),
+        }
+    }
+}
--- src/config/config.rs
+++ src/config/config.rs
@@ -1,7 +1,14 @@
+use std::{io::Write, path::PathBuf};
+
+use anyhow::Result;
+
+use super::app_config::AppConfig;
+
 pub struct Config {
     config_file: PathBuf,
     config_directory: PathBuf,
     data_directory: PathBuf,
+    app_config: AppConfig,
 }
 
 impl Config {
@@ -20,12 +27,21 @@ impl Config {
 
             ensure_dir(&data_directory)?;
             ensure_dir(&config_directory)?;
-            ensure_file(&config_file)?;
+
+            let app_config: AppConfig = {
+                let file_content = ensure_file(
+                    &config_file,
+                    toml::to_string_pretty(&AppConfig::new_default()).unwrap(),
+                )?;
+
+                toml::from_str(&file_content)?
+            };
 
             Ok(Config {
                 config_file,
                 config_directory,
                 data_directory,
+                app_config,
             })
         }
     }
@@ -37,10 +53,12 @@ fn ensure_dir(dir: &PathBuf) -> Result<()> {
     Ok(())
 }
 
-fn ensure_file(file_path: &PathBuf) -> Result<()> {
+fn ensure_file(file_path: &PathBuf, default: String) -> Result<String> {
     if !file_path.exists() {
-        std::fs::File::create(file_path)?;
+        let mut file = std::fs::File::create(file_path)?;
+        file.write_all(&default.as_bytes());
+        Ok(default)
+    } else {
+        Ok(std::fs::read_to_string(file_path)?)
     }
-
-    Ok(())
 }
