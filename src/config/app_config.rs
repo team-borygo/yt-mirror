4d37a2649760ab0cc2b2b6cdee9b600820185511
Mateusz Koteja <soanvig@users.noreply.github.com>
2023-04-25 21:43:20 +0200

    feat: add ability to set config file path, allow to configure data directory (#19)

--- src/config/app_config.rs
+++ src/config/app_config.rs
@@ -1,5 +1,3 @@
-use std::path::PathBuf;
-
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize)]
@@ -7,6 +5,7 @@ pub struct AppConfig {
     pub(super) bookmark_files: Vec<String>,
     pub(super) target_dir: String,
     pub(super) tmp_dir: Option<String>,
+    pub(super) data_dir: Option<String>,
 }
 
 impl AppConfig {
@@ -14,6 +13,7 @@ impl AppConfig {
         AppConfig {
             bookmark_files: vec![],
             target_dir: "".to_string(),
+            data_dir: None,
             tmp_dir: None,
         }
     }
